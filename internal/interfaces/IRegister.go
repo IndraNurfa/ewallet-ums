95f4aedb72cd8f9182d410226bbc4915966bb6a0
Ahmad Indra Nurfauzi <ahmad.nurfauzi@moladin.com>
2025-06-27 15:33:50 +0700

    feat: add user logout

--- internal/interfaces/IRegister.go
+++ internal/interfaces/IRegister.go
@@ -3,12 +3,17 @@ package interfaces
 import (
 	"context"
 	"ewallet-ums/internal/models"
+
+	"github.com/gin-gonic/gin"
 )
+
+type IRegisterHandler interface {
+	Register(c *gin.Context)
+}
+type IRegisterService interface {
+	Register(ctx context.Context, request models.User) (interface{}, error)
+}
 
 type IRegisterRepository interface {
 	InsertNewUser(ctx context.Context, user *models.User) error
-}
-
-type IRegisterService interface {
-	Register(ctx context.Context, request models.User) (interface{}, error)
 }
